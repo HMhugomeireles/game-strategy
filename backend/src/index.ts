dbb9cb7f15571e7e4543f70fcf101a6cedd37fd7
Hugo Meireles <meireles.hmm@gmail.com>
2019-05-15 01:39:37 +0100

    remove test middleware

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -3,10 +3,7 @@ import express = require('express');;
 const app = express();
 const port = 8080; // default port to listen
 
-function middleware(req: express.Request, res: express.Response, next: any){
-    console.log(`${req.method} ${req.path}`);
-    next();
-}
+
 
 // define a route handler for the default home page
 app.get( "/", ( req, res ) => {
