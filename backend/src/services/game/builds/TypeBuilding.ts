a71b480de845f95f6c285f2dd92b78803627e577
Hugo Meireles <meireles.hmm@gmail.com>
2019-05-31 01:08:37 +0100

    enum types

---
+++ backend/src/services/game/builds/TypeBuilding.ts
@@ -0,0 +1,12 @@
+enum TypeBuilding {
+  BASE,
+  CARPENTRY,
+  SLAUGHTER_HOUSE,
+  IRON_HOUSE,
+  REFINERY,
+  QUARRY,
+  WATER_WELL,
+  ARMY_HOUSE
+}
+
+export default TypeBuilding;
---
+++ backend/src/services/game/characters/typeCharacter.ts
@@ -0,0 +1,12 @@
+enum TypeCharacter {
+  WORKER,
+  INFANTRY,
+  ARMY_I,
+  ARMY_II,
+  ARMY_III,
+  SPECIAL_FORCE,
+  ASSAULT_TROOP,
+  SEAL
+}
+
+export default TypeCharacter;
