eb5e55fa12269d27a2c40b749b051b56c562559e
Hugo Meireles <meireles.hmm@gmail.com>
2021-06-13 22:47:31 +0100

    add packages helmet cors to middleware

--- backend/src/main/middlewares/setupMiddleware.ts
+++ backend/src/main/middlewares/setupMiddleware.ts
@@ -1,5 +1,9 @@
 import { Express, json } from 'express'
+import helmet from 'helmet'
+import cors from 'cors'
 
 export const setupMiddlewares = (app: Express): void => {
+  app.use(helmet());
+  app.use(cors());
   app.use(json());
 }
